677c0f42ee252797aa9d8fa4a9787ce338d80dff
Khem Khem <khem6333@gmail.com>
2024-09-24 10:08:36 +0100

    Update email for registeration

--- app/emailService/mail_templates/events.ts
+++ app/emailService/mail_templates/events.ts
@@ -17,7 +17,7 @@ export const generateEventRegisterHTMLTemp = (option: { name?: string }) => {
       <li>End Date: 5th Oct 2024</li>
       <li>Certification Day: 19th Oct 2024 (Physical)</li>
       <li>Focus Areas: Cybersecurity and Web Development for Remote Work</li>
-      <li>Venue Google Meet: Sessions will generally run from 4:00 PM to 6:00 PM WAT, with some exceptions. You will receive the link to join each day. However, if you joined the program after September 24th, feel free to use the following details to join the sessions:</li>
+      <li>Venue Google Meet: Sessions will generally run from 4:00 PM to 6:00 PM WAT, with some exceptions. You will receive the link to join each day. However, if you joined the program on or after September 24th, feel free to use the following details to join the sessions:</li>
     </ul>
 
     <h4>
